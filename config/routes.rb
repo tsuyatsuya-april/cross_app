420976bcda9c99ff174b117d4c62a979b031bd4a
tsuyatsuya-april <ka5meka5mewith871monme@gmail.com>
2020-12-25 10:25:30 +0900

    編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'cross_answers#index' 
   resources :students, only: [:index, :new, :create]
   resources :lessons, only: [:index, :new, :create]
-  resources :cross_answers, only: [:index, :create]
+  resources :cross_answers, only: [:index, :create, :update, :destroy]
   resources :members, only: [:new, :create]
   resources :battings do
     collection do
